7c4d5357c229855d97715e03ba059cf019ce725d
Pablo Blanco Celdrán <pablobc.1995@gmail.com>
2020-12-15 00:47:00 +0100

    Cube spins. :v

--- src/nativeMain/kotlin/com/argochamber/horizonengine/main.kt
+++ src/nativeMain/kotlin/com/argochamber/horizonengine/main.kt
@@ -12,11 +12,15 @@ val cam = PerspectiveCamera(90f.toRadians(), 1024f / 768f, .1f, 100f)
 
 class Test(private val model: Model, private val shader: Shader) : Spatial(), Drawable {
     var tint = Vector.WHITE
+    var i = 0f
     override fun draw() {
         shader.bind()
         shader["MVP"].set(cam.project(globalTransform))
         shader["tint"].set(tint)
         model.draw()
+        cam.position.x = kotlin.math.sin(i) * 3f
+        cam.position.z = kotlin.math.cos(i) * 3f
+        i += 0.005f
     }
 }
 
@@ -25,9 +29,9 @@ fun main() {
     engine.createWindow(1024, 768, "Horizon Engine v1.0").let { result ->
         if (result !== Engine.WindowResult.Ok) error(result.message)
     }
-    cam.position.x = .3f
-    cam.position.y = .3f
-    cam.position.z = .3f
+    cam.position.x = 3f
+    cam.position.y = 3f
+    cam.position.z = 3f
     val shader = Shader.compile(
         """
         #version 330 core
