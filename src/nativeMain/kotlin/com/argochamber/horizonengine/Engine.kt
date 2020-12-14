178fa98dfce087b411e46809935a99e5d3facaf8
Pablo Blanco Celdrán <pablobc.1995@gmail.com>
2020-12-14 13:52:31 +0100

    Reworked engine warmup pipeline.

--- src/nativeMain/kotlin/com/argochamber/horizonengine/Engine.kt
+++ src/nativeMain/kotlin/com/argochamber/horizonengine/Engine.kt
@@ -1,4 +1,38 @@
 package com.argochamber.horizonengine
 
-object Engine {
+import com.argochamber.horizonengine.graphics.Drawable
+
+/**
+ * Engine central wrapper.
+ */
+class Engine private constructor() {
+    companion object {
+        /**
+         * Creates a window or throws if could not.
+         */
+        fun create(w: Int, h: Int, title: String) = when (horizon.createWindow(w, h, title)) {
+            horizon.WindowCreationCodes.FAILED_GLEW_INITIALIZATION -> error("Failed to initialize GLEW, aborting...")
+            horizon.WindowCreationCodes.FAILED_GLFW_INITIALIZATION -> error("Failed to initialize GLFW, aborting...")
+            horizon.WindowCreationCodes.INCOMPATIBLE_PROFILE_MODE -> error("Incompatible GPU profile mode, aborting...")
+            else -> Engine()
+        }
+    }
+
+    private val drawQueue = mutableListOf<Drawable>()
+
+    fun enableDraw(drawable: Drawable) {
+        drawQueue.add(drawable)
+    }
+
+    /**
+     * Starts the game loop.
+     */
+    fun start() {
+        horizon.setupInput()
+        do {
+            horizon.clear()
+            drawQueue.forEach(Drawable::draw)
+            horizon.processEvents()
+        } while (horizon.shouldNotExit() != 0)
+    }
 }
--- src/nativeMain/kotlin/com/argochamber/horizonengine/main.kt
+++ src/nativeMain/kotlin/com/argochamber/horizonengine/main.kt
@@ -5,18 +5,8 @@ import com.argochamber.horizonengine.graphics.Model
 import com.argochamber.horizonengine.graphics.Shader
 import com.argochamber.horizonengine.math.toRadians
 import com.argochamber.horizonengine.math.Vector
-import com.argochamber.horizonengine.scene.Node
 import com.argochamber.horizonengine.scene.PerspectiveCamera
 import com.argochamber.horizonengine.scene.Spatial
-
-class Game {
-    var root = Node()
-    val drawQueue = mutableListOf<Drawable>()
-
-    fun draw() {
-        drawQueue.forEach { it.draw() }
-    }
-}
 
 val cam = PerspectiveCamera(90f.toRadians(), 1024f / 768f, .1f, 100f)
 
@@ -30,7 +20,8 @@ class Test(private val model: Model, private val shader: Shader) : Spatial(), Dr
     }
 }
 
-fun start() {
+fun main() {
+    val engine = Engine.create(1024, 768, "Agachuu y anchoas de playa")
     cam.position.x = 3f
     cam.position.y = 3f
     cam.position.z = 3f
@@ -52,22 +43,7 @@ fun start() {
         }
         """) ?: error("Could not compile the shader.")
     val model = Model.load("assets/triangle.mdl")
-    val game = Game()
     val test = Test(model, shader)
-    game.drawQueue.add(test)
-    horizon.setupInput()
-    do {
-        horizon.clear()
-        game.draw()
-        horizon.processEvents()
-    } while (horizon.shouldNotExit() != 0)
-}
-
-fun main() {
-    when (horizon.createWindow(1024, 768, "Agachuu y anchoas de playa")) {
-        horizon.WindowCreationCodes.FAILED_GLEW_INITIALIZATION -> error("Failed to initialize GLEW, aborting...")
-        horizon.WindowCreationCodes.FAILED_GLFW_INITIALIZATION -> error("Failed to initialize GLFW, aborting...")
-        horizon.WindowCreationCodes.INCOMPATIBLE_PROFILE_MODE -> error("Incompatible GPU profile mode, aborting...")
-        horizon.WindowCreationCodes.WINDOW_CREATION_OK -> start()
-    }
+    engine.enableDraw(test)
+    engine.start()
 }
